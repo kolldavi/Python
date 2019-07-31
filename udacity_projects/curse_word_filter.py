2765c50c1062b9367d1688ece3e30246bda08e56
kolldavi <kolldavi@gmail.com>
2019-07-30 22:39:53 -0500

    remove twillio

--- udacity_projects/curse_word_filter.py
+++ udacity_projects/curse_word_filter.py
@@ -1,20 +1,3 @@
-# # Download the helper library from https://www.twilio.com/docs/python/install
-# from twilio.rest import Client
-
-
-# # Your Account Sid and Auth Token from twilio.com/console
-# # DANGER! This is insecure. See http://twil.io/secure
-# account_sid = 'AC81443fb744fc4a8701f8657b9f70b8a2'
-# auth_token = 'a1abdca177de368b19c31e6a85998337'
-# client = Client(account_sid, auth_token)
-
-# message = client.messages.create(
-#                               body='Hi there!',
-#                               from_='+17083989253',
-#                               to='+17087179049'
-#                           )
-
-# print(message.sid)
 from urllib import request, parse
 
 def read_text():
@@ -27,9 +10,9 @@ def read_text():
 
 
 def check_curse_word(txt):
-    url = "http://www.wdylike.appspot.com/?q="
-    connection = url + parse.quote(txt)
-    req = request.urlopen(connection)
+    url = f'http://www.wdylike.appspot.com/?q={parse.quote(txt)}'
+    
+    req = request.urlopen(url)
     answer = req.read()
     print(answer)
     req.close()
